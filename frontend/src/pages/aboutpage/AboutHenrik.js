3ee9ea4dc7bf54be698a9fa8ebacb0ce8215a358
Henrikc007 <128123648+Henrikc007@users.noreply.github.com>
2025-06-27 17:15:04 +0200

    m

--- frontend/src/pages/aboutpage/AboutHenrik.js
+++ frontend/src/pages/aboutpage/AboutHenrik.js
@@ -1,3 +1,5 @@
 export default function Aboutme() {
-    return <p>This is about Henrik Christensen and the goal to make youre the lucky finder of a 4-clover.</p>;
+    return( 
+    <p> My name is Henrik Christensen, a mathematician/phycicist and also a programmer. <br />
+    This is about Henrik Christensen and the goal to make youre the lucky finder of a 4-clover. </p> );
 }
