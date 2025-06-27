2fa5206e761221dec67dc7fa5e828efdd26ff910
Henrikc007 <128123648+Henrikc007@users.noreply.github.com>
2025-06-27 18:27:54 +0200

    m

---
+++ frontend/src/pages/donatepage/helpapreciated.js
@@ -0,0 +1,4 @@
+export default function Helpisapreciated({name="Guest"}) {
+    return( 
+    <p> Dear {name}, I thank you for thinking about donating to the development of this app and the content on this page</p> );
+}
