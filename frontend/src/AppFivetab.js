9eea4514f30eff243ed6bfc0f9021f5fb171a1a2
Henrikc007 <128123648+Henrikc007@users.noreply.github.com>
2025-06-27 16:00:18 +0200

    m

--- frontend/src/AppFivetab.js
+++ frontend/src/AppFivetab.js
@@ -1,6 +1,6 @@
 // SidebarLayout.js
 import React, { useState } from "react";
-import AboutHenrik from "./src/pages/aboutpage/AboutHenrik";
+import AboutHenrik from "frontend\src\pages\aboutpage\AboutHenrik.js";
 const tabs = [
   { id: "hjem", label: "Hjem" },
   { id: "profile", label: "Profile" },
